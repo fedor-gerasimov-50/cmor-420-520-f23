acc4b86b7fcddb6895105b1f6c6553febd521356
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2023-09-06 10:29:33 -0500

    Update shell_script_with_args.sh

--- linux_codes/shell_script_with_args.sh
+++ linux_codes/shell_script_with_args.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# this checks if there is a directory with the same name as the command line argument $1 
 if [ ! -d "$1" ]; then
     echo "$1 is a directory that does not already exist."
 fi
