a03d410d05efb6f8bddc4f2371487f169dc2fd30
Jesse Chan <jesse.lee.chan@gmail.com>
2023-09-20 11:38:28 -0500

    updating demo

--- c_codes/static_arrays.c
+++ c_codes/static_arrays.c
@@ -3,29 +3,23 @@
 
 // This file illustrates some differences between
 // dynamic and statically defined arrays.
-
-// (*x)[2] -> dereferencing x gives an array of size 2
-void foo(int (*x)[2]){
-  printf("x[0] = %d, %d\n", x[0][0], x[0][1]);
-  printf("x[1] = %d, %d\n", x[1][0], x[1][1]);  
+void foo(int (*x)[2], char * msg){
+  printf("%s[0][0] = %d\n", msg, x[0][0]);
 }
 
 int main(void){
 
-  int x[2][2] = {{1,2}};
-  foo(x);
+  // static array
+  int x[10][2] = {{13, 1}, {2, 3}, {1, 2}};
 
-  // how to cast a statically defined array? use (*) without the variable declaration
-  int (*y)[2] = (int (*)[2]) x;
-  y[0][1] = 100;
-
-  printf("y = %d, %d\n", y[0][0], y[0][1]);
-  printf("x = %d, %d\n", x[0][0], x[0][1]);  
-
-  /*
-    void * z = malloc(sizeof(int) * 4);
-    int * w = NULL;
-    printf("z[0] = %d\n", ((int *)z)[0]);
-  */
+  // dynamic array  
+  int ** y = (int **) malloc(sizeof(int *) * 2);
+  y[0] = (int *) malloc(sizeof(int) * 2);
+  y[1] = (int *) malloc(sizeof(int) * 2);
+  y[0][0] = 13;
+  
+  foo(x, "x");
+ 
+  // similarities
   return 0;
 }
