b0548b205a50245ab1d699364e395775a4f1144f
Jesse Chan <jesse.lee.chan@gmail.com>
2023-09-22 11:12:30 -0500

    update

--- c_codes/null_and_void_pointers.c
+++ c_codes/null_and_void_pointers.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 
 // This file illustrates the use of null and void pointers.
-
 int main(void){
 
+  int * nullptr = NULL;
+  printf("The null pointer address is %p\n", nullptr);
+  // *nullptr will throw an error
+    
+  // "malloc" returns (void *) = void pointer. We cast it
+  // explicitly to a typed pointer for C++ compatibility.
   void * x = malloc(sizeof(int) * 4);
-  int * y = NULL;
+  ((float *)x)[0] = 1;
   printf("x[0] = %d\n", ((int *)x)[0]);
+
   return 0;
 }
