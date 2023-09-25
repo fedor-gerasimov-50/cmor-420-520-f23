9d2f878e694d3bc46d37b393eeeb5fffd75170d7
Jesse Chan <jesse.lee.chan@gmail.com>
2023-09-25 10:44:17 -0500

    add struct demo

---
+++ c_codes/structs.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Vector{
+  int n;
+  double * ptr;
+} Vector;
+
+Vector init_vector(int n){
+  double * ptr = (double *) malloc(sizeof(double) * n);
+  Vector vector = {.n = n, .ptr = ptr};
+  return vector;
+}
+
+void set_index_Vector(Vector * x,
+		      double value,
+		      int index){
+  x->ptr[index] = value;
+}
+
+int main(void){
+
+  int n = 5;
+  Vector x = init_vector(n);
+
+  set_index_Vector(&x, 13.0, 1);
+
+  for (int i = 0; i < x.n; ++i){
+    printf("x.ptr[%d] = %f\n", i, x.ptr[i]);
+  }
+}
