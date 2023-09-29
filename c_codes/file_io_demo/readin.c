27a0594df347df3ff3a5c363ffd9a233599f496c
Jesse Chan <jesse.lee.chan@gmail.com>
2023-09-29 10:59:49 -0500

    update demos

--- c_codes/file_io_demo/readin.c
+++ c_codes/file_io_demo/readin.c
@@ -1,17 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // This demo is an example of how to use fgets to read
 // in ASCII input from a file.
 
 int main(int argc, char ** argv){
+
+  FILE * fp = fopen("in.txt", "r");
   
-  char buff[BUFSIZ];
-  FILE *fp = fopen("in.txt", "r");
-  fgets(buff, BUFSIZ, fp);
-  printf("First line read by fgets: %s\n", buff);
-  fgets(buff, BUFSIZ, fp);
-  printf("Second line read by fgets: %s\n", buff);
+  char buffer[BUFSIZ];
+
+  fgets(buffer, BUFSIZ, fp);
+  printf("First line read by fgets: %s\n", buffer);
+  
+  fgets(buffer, BUFSIZ, fp);
+  printf("Second line read by fgets: %s\n", buffer);
+  
   fclose(fp);
-  
+  /*
+  int x;
+  char c;
+  printf("Enter an int and a char, separated "
+	 "by a space:\n");	 
+  scanf("%d %c", &x, &c);
+  printf("Scanned variables: x = %d, c = %c\n", x, c);
+  */
+
+  char str[BUFSIZ];
+  printf("Enter a single word:\n");
+  scanf("%s", str);
+  printf("The word entered is: %s\n", str);
+
+  int day, month;
+  char year_end[2];  
+  char tmp[BUFSIZ];
+
+  sscanf("Date: 9/29/23",
+	 "%s %d/%d/%s", tmp, &day, &month, year_end);
+
+  char year[BUFSIZ];
+  sprintf(year, "20%s", year_end);
+  printf("Day, month, year = %d, %d, %s\n",
+	 day, month, year);
+
 }
--- c_codes/file_io_demo/writeout.c
+++ c_codes/file_io_demo/writeout.c
@@ -3,33 +3,38 @@
 
 // This file is an example of how to write output
 // to a file.
-int main(int argc, char ** argv){
-  FILE *fp;
+int main(void){
 
-  // open an output file
-  fp = fopen("out.txt", "w+");
-  fprintf(fp, "Writing output using fprintf...\n");
-  char * string = "Writing output using fputs...\n";
-  fputs(string, fp);
+  // print ASCII formatted output to file
+  FILE * fp = fopen("out.txt", "w");
+
+  int n = 10;
+  double * x = (double *) malloc(sizeof(double) * n);
+  for (int i = 0; i < n; ++i){
+    fprintf(fp, "%f\n", x[i]);
+  }
+
   fclose(fp);
 
-  // now write a block of memory to a binary file
-  fp = fopen("out.bin", "w+");
-  int n = 1;
-  int * x = (int *) malloc(sizeof(int) * n);
+  // printing binary to file
+  fp = fopen("out.bin", "w");
+
+  // write out x[0] = 13
   x[0] = 13;
-  fwrite(x, sizeof(int), n, fp);
-  fclose(fp);
+  fwrite(x, sizeof(double), n, fp);
 
-  // change x[0]
+  fclose(fp);
+  
+  // modify x[0]
   x[0] = 42;
 
-  // re-read out.bin into x
-  fp = fopen("out.bin", "r");    
-  fread(x, sizeof(int), n, fp);
-  fclose(fp);
+  // read in x[0] again to over-write x[0]
+  fp = fopen("out.bin", "r");  
+  fread(x, sizeof(double), n, fp);  
+  fclose(fp);  
 
-  printf("x[0] = %d\n", x[0]);
+  printf("x[0] = %f\n", x[0]); 
 
   
+  return 0;
 }
