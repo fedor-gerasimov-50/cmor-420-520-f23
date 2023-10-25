d46be68d392137b1541e27f20b64083c7ddac133
Jesse Chan <jesse.lee.chan@gmail.com>
2023-10-25 11:03:08 -0500

    update derived.cpp

--- cpp_codes/derived.cpp
+++ cpp_codes/derived.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
 
 class Base{
-public:
-  // virtual = default print() function
-  virtual void print(){
+public: 
+  void print(){
     std::cout << "Inside base class" << std::endl;
   };
 };
 
-class DerivedFromBase : public Base{
+class DerivedFromBase : public Base {
 public:
   void print(){
-    std::cout << "running print() in class `DerivedFromBase`" << std::endl;
-  }  
+    std::cout << "Inside the derived class" << std::endl; 
+  } 
 };
 
 int main(void){
-
-  Base base;
-  base.print();
   
   DerivedFromBase foo;
 
-  foo.print();
-
-  // we can create pointers to base classes
-  // but assign the address of a derived class.
-  Base * baz;
-  baz = &foo;
-  baz->print();
-
+  return 0;
 }
